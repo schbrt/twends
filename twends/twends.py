2f43627605addd4ed9c848616d7415e5379ec5aa
Michael Schubert <schubert.michael.j@gmail.com>
2014-05-11 02:06:11 -0500

    added methods to find recent trends, get related tweets

--- twends/twends.py
+++ twends/twends.py
@@ -9,13 +9,32 @@ except ImportError:
 def connect_api():
     keys = secret_pass.secret_dict()
     auth = tweepy.auth.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
-    auth.set_access_token(keys['access_key'], keys['access_secret'])
+    #auth.set_access_token(keys['access_key'], keys['access_secret'])
     api = tweepy.API(auth)
     return api
 
+def get_tweets(api, tag):
+    """
+    Returns queue of tweets with a given hashtag
+    """
+    tweets = api.search(tag)
+    print len(tweets)
+
+def get_trends(api, loc=1):
+    """
+    Returns a list of trending topics for the given location.
+    Default location is WOEID = 1, worldwide
+    """
+    trend_dict = api.trends_place(loc)
+    data = trend_dict[0]
+    trend_list = data['trends']
+    tags = [trend['name'] for trend in trend_list]
+    return tags
+
 def main():
     api = connect_api()
-    trends = api.trends_place(1)
-
+    trends = get_trends(api,23424977)
+    for tag in range(len(trends)):
+        get_tweets(api, trends[tag])
 if __name__ == '__main__':
     main()
